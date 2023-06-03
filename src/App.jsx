34ac1ef058abe4425e6b3d2fe0dd9f5aaeb1f4e2
Raja Babu <rb4647184@gmail.com>
2023-06-03 12:33:56 +0530

    playagain link problem

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   <Navbar />
   <Routes>
     <Route path='/' element={<Home />} />
-    <Route path='/Playagain' element={<Playagain />} />
+    <Route path='/playagain' element={<Playagain />} />
   </Routes>
   </BrowserRouter>
     </>
